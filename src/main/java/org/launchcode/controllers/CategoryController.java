ae197e26896f22530f503a79bb1b67e30ae64127
kennethkeefer1 <43766735+kennethkeefer1@users.noreply.github.com>
2019-02-14 21:44:29 -0600

    Update CategoryController.java

--- src/main/java/org/launchcode/controllers/CategoryController.java
+++ src/main/java/org/launchcode/controllers/CategoryController.java
@@ -1,12 +1,49 @@
-package org.launchcode.models.data;
+package org.launchcode.controllers;
 
-import org.launchcode.models.Menu;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
+import org.launchcode.models.Category;
+import org.launchcode.models.data.CategoryDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.transaction.Transactional;
+import javax.validation.Valid;
 
-@Repository
-@Transactional
-public interface MenuDao extends CrudRepository<Menu, Integer> {
+@Controller
+@RequestMapping("category")
+public class CategoryController {
+
+    @Autowired
+    private CategoryDao categoryDao;
+
+    @RequestMapping(value="", method=RequestMethod.GET)
+    public String index(Model model, @RequestParam(defaultValue = "0") int id) {
+        model.addAttribute("title", "Categories");
+        model.addAttribute("categories", categoryDao.findAll());
+        return "category/index";
+    }
+
+    @RequestMapping(value="add", method = RequestMethod.GET)
+    public String add(Model model) {
+        model.addAttribute(new Category());
+        model.addAttribute("title", "Add Category");
+        return "category/add";
+    }
+
+    @RequestMapping(value="add", method= RequestMethod.POST)
+    public String add(Model model, @ModelAttribute @Valid Category category, Errors errors) {
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Add Category");
+            return "redirect:category/add";
+        }
+
+        categoryDao.save(category);
+        return "redirect:";
+    }
+
+
 }
